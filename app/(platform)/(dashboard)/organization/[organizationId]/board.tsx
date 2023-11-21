969072deaed376799583fdb836de1ff7039c68d3
LEETAEUNG <taewoong@hangloo.co.kr>
2023-11-21 22:16:42 +0900

    글 추가 ,삭제 , component 분리

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,5 +1,6 @@
 import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
+import { FormDelete } from "./form-delete";
 
 interface BoardProps {
     title : string;
@@ -13,13 +14,11 @@ export const Board = ({
     const deleteBoardWithId = deleteBoard.bind(null, id);
 
     return (        
-            <form action={deleteBoardWithId} className="flex items-center gap-x-2">
-                <p>
-                    Board title : {title}
-                </p>
-                <Button type="submit" variant="destructive" size="sm">
-                    Delete
-                </Button>
-            </form>        
+        <form action={deleteBoardWithId} className="flex items-center gap-x-2">
+            <p>
+                Board title : {title}
+            </p>
+            <FormDelete/>
+        </form>        
     )
 }
