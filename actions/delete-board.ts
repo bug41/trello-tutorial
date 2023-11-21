8d2bf3ae318b2c69ad943b81418c85007813c05c
LEETAEUNG <taewoong@hangloo.co.kr>
2023-11-21 21:53:13 +0900

    board 기능 추가

---
+++ actions/delete-board.ts
@@ -0,0 +1,16 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+    
+export async function deleteBoard(id: string){    
+
+    await db.board.delete({
+        where : {
+            id
+        }
+    })
+
+    //real-time
+    revalidatePath("/organization/org_2YLUdSLumH9zbT5be3MdIWJgith")
+}
